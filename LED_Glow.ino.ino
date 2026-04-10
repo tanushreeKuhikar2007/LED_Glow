7196ebf0ec7e7375c061538cc10ddb91235f7a82
tanushreeKuhikar2007 <ugf24er054@jitnagpur.edu.in>
2026-04-10 14:01:18 +0530

    Add files via upload

---
+++ LED_Glow.ino.ino
@@ -0,0 +1,11 @@
+#define my_led 7
+
+void setup() {
+ pinMode(my_led,OUTPUT) ;// put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(my_led,HIGH);// put your main code here, to run repeatedly:
+
+}
